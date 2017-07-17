e0c248209bf0c61ecb8002f39be82d25523d7e6e
GroupPluginer <4gamesl7o@gmail.com>
2017-07-17 16:22:13 +0300

    Create Main.php

---
+++ src/Heart/Main.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Heart;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\level\particle\HeartParticle;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implenets Listener{
