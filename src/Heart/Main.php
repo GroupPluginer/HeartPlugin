b0e39768443729ef4a4e03286d9a4ea53f5923ba
GroupPluginer <4gamesl7o@gmail.com>
2017-07-17 16:27:21 +0300

    Update Main.php

--- src/Heart/Main.php
+++ src/Heart/Main.php
@@ -6,5 +6,14 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\level\particle\HeartParticle;
 use pocketmine\event\Listener;
+use pocketmine\permission\Permission;
 
-class Main extends PluginBase implenets Listener{
+class Main extends PluginBase implements Listener{
+
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onMove(PlayerMoveEvent $event){
+    if($player->hasPermission("Heart.particle")(({
+      
